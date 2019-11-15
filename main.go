9f4257e9a07ea3365a8618d049166d16f70d3ad9
gaotong <bladerework@gmail.com>
2019-11-15 10:25:39 +0800

    test 11

--- main.go
+++ main.go
@@ -4,12 +4,8 @@ import (
   "github.com/gin-gonic/gin"
   "github.com/sirupsen/logrus"
 )
-
-
-
 func main() {
   r := gin.Default()
-
   r.GET("/health", func(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H {
       "health": true,
